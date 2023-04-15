059cd2a4954ba30dc3e0425d75b176a292a608e9
Sonal Pirag <130848375+SonalPirag@users.noreply.github.com>
2023-04-15 19:56:18 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,11 @@
+class array
+{
+public static void main(String args[])
+{
+String Str[] = {"indore","bhopal","dewas", "ujjain","dhar","india",indonesa","bhind"};
+for(int i=0;i<Str.length;i++)
+{
+System.out.print(Str.index of ('ind');
+}
+}
+}
